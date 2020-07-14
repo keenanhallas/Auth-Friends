7bf83f327a2d711d86ce5a8bd434ca91afdb46e8
Keenan <keenanhallas@gmail.com>
2020-07-14 16:14:17 -0400

    axiosWithAuth written and being used to access API

--- friends/src/components/FriendsList.js
+++ friends/src/components/FriendsList.js
@@ -1,7 +1,30 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { axiosWithAuth } from "../utils/axiosWithAuth";
 
 export const FriendsList = () => {
+    const [friends, setFriends] = useState([]);
+
+    useEffect(() => {
+        axiosWithAuth()
+            .get("friends")
+            .then(res => {
+                console.log(res.data);
+                setFriends(res.data);
+            })
+            .catch(err => console.log(err))
+    }, []);
+
     return (
-        <div>This will be a friends list</div>
+        <div>
+            <div className="friendsList">
+                {friends.map(friend => {
+                    return (
+                        <div>
+                            Test card
+                        </div>
+                    );
+                })}
+            </div>
+        </div>
     );
 }
